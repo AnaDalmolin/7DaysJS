a2d7f6cdd3af82b0bdc3955bf5ce8320b7dfb4ca
AnaDalmolin <anadv21@hotmail.com>
2023-10-21 13:54:44 -0300

    Exercicio02 Opcional

---
+++ Exercicio02Opcional.js
@@ -0,0 +1,12 @@
+const prompt = require("prompt-sync")();
+
+var linguagem = "JavaScript";
+
+var resposta = prompt("Você gosta de estudar" + linguagem +"? Responda com o número 1 para SIM ou 2 para NÃO.");
+
+    if(resposta == 1){
+        console.log("Muito bom! Continue estudando e você terá muito")
+    }else{
+        console.log("Ahh que pena... Já tentou aprender outras linguagens?")
+    }
+
